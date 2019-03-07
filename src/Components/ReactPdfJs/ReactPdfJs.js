8635349c5425e4a91a0b8261a7d14c013e1faf4f
Matias Codina <matcod86@gmail.com>
2019-03-07 11:03:41 -0300

    Static version of pdfjs-dist

--- src/Components/ReactPdfJs/ReactPdfJs.js
+++ src/Components/ReactPdfJs/ReactPdfJs.js
@@ -1,6 +1,26 @@
 import React from "react";
+import PDF from "pdfjs-dist";
 
 export default class ReactPdfJs extends React.Component {
+  state = {
+    pdf: {},
+    testPdf: "files/quiroga.pdf",
+    workerSrc:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js"
+  };
+
+  componentDidMount() {
+    // Load the file
+    // console.log(PDF);
+    // console.log(this.state.testPdf);
+    PDF.GlobalWorkerOptions.workerSrc = this.state.workerSrc;
+
+    var loading = PDF.getDocument({
+      url: this.state.testPdf
+    });
+    console.log(loading);
+  }
+
   render() {
     return <div>PDF.JS</div>;
   }
