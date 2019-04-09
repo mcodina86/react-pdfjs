8268dc69f41d7c7c6af8617c227b815bff108698
Matias Codina <matcod86@gmail.com>
2019-04-08 21:25:25 -0300

    Progress bar added

--- src/Components/ReactPdfJs/components/Progressbar.js
+++ src/Components/ReactPdfJs/components/Progressbar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Progressbar = props => {
   const { loading } = props;
-
-  return <div>{loading}% loaded</div>;
+  const style = {
+    width: loading + "%"
+  };
+  return (
+    <div className="progressbar">
+      {loading < 100 ? (
+        <span className="progressbar__bar" style={style} />
+      ) : null}
+      {loading < 100 ? (
+        <span className="progressbar__text">{loading}% loaded</span>
+      ) : null}
+    </div>
+  );
 };
 
 export default Progressbar;
